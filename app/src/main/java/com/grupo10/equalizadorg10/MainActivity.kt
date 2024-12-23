0ed1183792f1778173795647c42509df8c400914
Gabrielle Desiree <gabrielle.desiree@gmail.com>
2024-12-23 19:11:53 -0300

    Layout main screen

--- app/src/main/java/com/grupo10/equalizadorg10/MainActivity.kt
+++ app/src/main/java/com/grupo10/equalizadorg10/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.grupo10.equalizadorg10.ui.theme.EqualizadorG10Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +33,76 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EqualizadorG10Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+
+                    EqualizadorApp(
+
                     )
+
                 }
             }
-        }
+
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun EqualizadorMainScreen(modifier: Modifier = Modifier) {
+
+    Column(modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Image(
+            painter = painterResource(R.drawable.audio_waves),
+            contentDescription = "Audio waves icon"
+        )
+
+
+        Text(
+            text = "G10",
+            fontSize = 55.sp,
+            fontFamily = FontFamily.SansSerif,
+            modifier = Modifier
+                .padding(5.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+
+        )
+
+        Text(
+            text = "Equalizador",
+            fontSize = 20.sp,
+            fontFamily = FontFamily.SansSerif,
+            /* TODO: change Color*/
+            modifier = Modifier
+
+                .align(alignment = Alignment.CenterHorizontally)
+
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        ElevatedButton(onClick ={/* TODO: implement profile screen */ }){
+            Text(text = "Profile 1")
+        }
+        ElevatedButton(onClick ={/* TODO: implement profile screen */ }){
+            Text(text = "Profile 2")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        FilledTonalButton(onClick ={/* TODO: implement profile screen */ }){
+            Text(text = "Add new profile")
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun EqualizadorApp(){
     EqualizadorG10Theme {
-        Greeting("Android")
+        EqualizadorMainScreen(modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+        )
     }
 }
